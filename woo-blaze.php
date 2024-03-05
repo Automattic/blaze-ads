6289036bd444e43ddab14b151112f03571f4f552
Sebastián Barbosa <sebabarbosa@gmail.com>
2024-03-05 12:32:52 -0300

    Revert "add translation hook" (#35)

--- woo-blaze.php
+++ woo-blaze.php
@@ -6,7 +6,6 @@
  * Version: 0.0.6
  * Author: Automattic
  * Author URI: https://automattic.com/
- * Woo: 5278104:bf3cf30871604e15eec560c962593c1f
  * Text Domain: woo-blaze
  * Domain Path: /languages
  * Requires at least: 6.3
@@ -26,9 +25,6 @@ $is_autoloading_ready = class_exists( Automattic\Jetpack\Connection\Rest_Authent
 if ( ! $is_autoloading_ready ) {
 	return;
 }
-
-add_filter( 'woocommerce_translations_updates_for_woo-blaze', '__return_true' );
-
 
 /**
  * Initialize the Jetpack functionalities: connection, etc.
